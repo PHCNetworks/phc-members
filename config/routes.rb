b7ccd3c0b88c5ec85dd7bee17c54f688dd382c10
Brad Potts <bradley.j.potts@gmail.com>
2016-03-07 16:45:18 +0000

    Fixes to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,34 @@
 Phcmembers::Engine.routes.draw do
 
-	if defined?(Mtdevise) constraints(Devise::Constraints::SubdomainRequired) do
+	if defined?(Mtdevise) 
+		constraints(Devise::Constraints::SubdomainRequired) do
 
-		# Application Client Backend
-		namespace :members do
-	
-			# Members Main Index
-			root 'mains#index'
-	
-			# Member Routes
-			resources :mains do 
-				resources :listings
-				resources :contacts
-			end
-	
-		end
-	
-		namespace :directory do
-	
-			# Directory Main Index
-			root 'categories#index'
-	
-			# Directory Routes
-			resources :categories do
-				resources :catlists
-			end
-		end
+			# Application Client Backend
+			namespace :members do
 		
+				# Members Main Index
+				root 'mains#index'
+		
+				# Member Routes
+				resources :mains do 
+					resources :listings
+					resources :contacts
+				end
+		
+			end
+	
+			namespace :directory do
+		
+				# Directory Main Index
+				root 'categories#index'
+		
+				# Directory Routes
+				resources :categories do
+					resources :catlists
+				end
+			end
+		end
+
 	end
 
 end
