43568001355312599a6b17fcbc67defce597a211
Brad Potts <bradley.j.potts@gmail.com>
2016-03-07 18:50:17 +0000

    Add contraints to routes file if mtdevise exists.

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,35 @@ Phcmembers::Engine.routes.draw do
 				end
 			end
 		end
+		
+	else
+		
+			# Application Client Backend
+			namespace :members do
+		
+				# Members Main Index
+				root 'mains#index'
+		
+				# Member Routes
+				resources :mains do 
+					resources :listings
+					resources :contacts
+				end
+		
+			end
+	
+			namespace :directory do
+		
+				# Directory Main Index
+				root 'categories#index'
+		
+				# Directory Routes
+				resources :categories do
+					resources :catlists
+				end
+			end
+		
+		
 
 	end
 
