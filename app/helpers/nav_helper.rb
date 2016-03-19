d3a1f8eca59f4055c28654dacc7f91f434982190
Brad Potts <bradley.j.potts@gmail.com>
2016-03-19 17:43:28 +0000

    Removed application helpers.

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -1,26 +1,29 @@
 module NavHelper
-  def nav_link(page, &link_text) 
-    class_name = request.env['PATH_INFO'].match(page) ? 'active' : nil
 
-    content_tag(:li, class: class_name) do
-      link_to page do
-        link_text.call
-      end
-    end
-  end
+	def nav_link(page, &link_text) 
+		class_name = request.env['PATH_INFO'].match(page) ? 'active' : nil
 
-  def treeview_link(text, &links)
-    content = link_to('#') do
-      content_tag(:span, text) <<
-      content_tag(:i, '', class: "fa pull-right fa-angle-left")
-    end
+		content_tag(:li, class: class_name) do
+			link_to page do
+				link_text.call
+			end
+		end
 
-    content << content_tag(:ul, class: 'treeview-menu') do 
-      links.call
-    end
+	end
 
-    content_tag :li, class: 'treeview' do
-      content
-    end
-  end
+	def treeview_link(text, &links)
+		content = link_to('#') do
+			content_tag(:span, text) <<
+			content_tag(:i, '', class: "fa pull-right fa-angle-left")
+		end
+
+		content << content_tag(:ul, class: 'treeview-menu') do 
+			links.call
+		end
+
+		content_tag :li, class: 'treeview' do
+			content
+		end
+	end
+
 end
