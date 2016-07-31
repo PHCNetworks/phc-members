fcc19f8ddde4c0ce3318325b937abdafc4d1c28d
Brad Potts <developers@phcnetworks.net>
2016-07-31 16:28:27 +0000

    fixes to listingcategory.

--- app/controllers/phcmembers/directory/categorylistings_controller.rb
+++ app/controllers/phcmembers/directory/categorylistings_controller.rb
@@ -5,7 +5,7 @@ module Phcmembers
 
 		# Security & Action Filters
 		before_action :set_paper_trail_whodunnit
-		before_action :set_directory_category, only: [:show, :edit, :update, :destroy]
+		before_action :set_directory_category, only: [:show]
 
 		# INDEX - Directory Category/Listings
 		def index
